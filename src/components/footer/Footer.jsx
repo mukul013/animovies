b9ce6ce838fb1e5e7174000c3a77c92dea7f4ffc
Mukul Rawat <mukul@Mukuls-MacBook-Air.local>
2023-09-14 13:52:21 +0530

    animovies project started

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                Animovies is your one-stop destination for all things cinema, meticulously crafted 
+                to satisfy your movie cravings and keep you in the loop with the latest and greatest 
+                in the world of film. Whether you're a die-hard movie buff or just looking for a great 
+                flick to watch, Animovies has got you covered with an array of exciting features and 
+                curated lists that will make your movie nights unforgettable.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
